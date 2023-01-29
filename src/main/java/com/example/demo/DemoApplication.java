0184fd2a16c7158728e7f94a501e47fe20c901c9
Dominik Köhler <dominik.koehler90@gmail.com>
2023-01-29 10:00:42 +0100

    add and display Student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.student.Student;
+
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 
 @SpringBootApplication
@@ -17,9 +21,17 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/")
-	public List<String> hello()
+	public List<Student> hello()
 	{
-		return List.of("Hello", "World");
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21
+				)
+		);
 	}
 
 }
