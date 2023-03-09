40bed8d7987a55b6957bfc2fca6f8b6126e515fd
Dominik Köhler <dominik.koehler90@gmail.com>
2023-03-09 16:42:16 +0100

    add annotations to use dependency injection

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class StudentController
 {
     private final StudentService studentService;
-
+    
+    @Autowired
     public StudentController(StudentService studentService)
     {
         this.studentService = studentService;
