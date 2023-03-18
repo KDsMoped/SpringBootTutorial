f8720eb1300823ac1ad18514d2a52743068b8d6f
Dominik Köhler <dominik.koehler90@gmail.com>
2023-03-18 11:49:24 +0100

    add delete mapping and service function

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,7 +5,9 @@ import java.util.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -31,6 +33,12 @@ public class StudentController
     public void registerNewStudent(@RequestBody Student student)
     {
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId)
+    {
+        studentService.deleteStudent(studentId);
     }
 
 }
